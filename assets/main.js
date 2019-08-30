c868f7b2ca4a40488e7245503e5c45cbf6e431bd
linnwergelius <35459728+linnwergelius@users.noreply.github.com>
2019-08-30 12:36:16 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -11,13 +11,13 @@
 
 $(document).ready(function() {
 	
-$("body").removeClass("bg")
+$("body").removeClass("bg")	
 $('.sliding').delay(700).removeClass('sliding');
-
 $('.hidden').fadeIn(300).removeClass('hidden');
-$(".header p").css("transform", "translate(0, 0)")
+$(".header p").css("transform", "translate(0, 0)")	
 $(".title p").css("transform", "translate(0, 0)")				
-$('.linkfade').fadeIn(1000);
+ $('.linkfade').fadeIn(1000);
+ 
 $(".about.button.inproject").addClass("back");
 
 });
@@ -81,58 +81,110 @@ $(".tohome2").click(function(){
 
 //SCROLL-EFFEKTER
 
-	$('.description').on('click',function(){
-		if($(this).attr('data-click-state') == 1) {
-		$(this).attr('data-click-state', 0)
-		$(".description p").fadeOut();	
-		$(this).delay(10000).removeClass("equalize")
-		$(".pictureslider").removeClass("equalize")	
-		$("span.cross").css("transform","rotate(0)")
-		$(".pictureslider").addClass("scrollable")
-		
-	
-	} else {
-		$(this).attr('data-click-state', 1)
-		$(this).addClass("equalize")
-		$(".pictureslider").addClass("equalize")
-		$(".pictureslider").removeClass("scrollable")
-		
-		
-		$("span.cross").css("transform","rotate(45deg)")
-			
-		$(".description p").delay(800).fadeIn();
-			$(".title").css("font-size","8vw")
-			$(".about").removeClass("shrink")
 
-		
-	
-	}
+
+	$('.description').on('click',function(){
+		//DESKTOP
+		if($(window).width()>900){
+			if($(this).attr('data-click-state') == 1) {
+				$(this).attr('data-click-state', 0)
+				$(".description p").fadeOut();	
+				$(this).delay(10000).removeClass("equalize")
+				$(".pictureslider").removeClass("equalize")	
+				$("span.cross").css("transform","rotate(0)")
+				$(".pictureslider").addClass("scrollable")
+			
+			} else {
+				$(this).attr('data-click-state', 1)
+				$(this).addClass("equalize")
+				$(".pictureslider").addClass("equalize")
+				$(".pictureslider").removeClass("scrollable")
+				$("span.cross").css("transform","rotate(45deg)")
+				$(".description p").delay(800).fadeIn();
+				$(".title").css("font-size","8vw")
+				$(".about").removeClass("shrink")
+    	
+				}
+			//MOBILE
+		} else {
+			
+			if($(this).attr('data-click-state') == 1) {
+				
+				$(this).attr('data-click-state', 0)
+				$(this).css("height","auto")
+				$(".description p").hide();	
+				$(".pictureslider").show();
+				
+				//$(this).delay(10000).removeClass("equalize")
+				//$(".pictureslider").removeClass("equalize")	
+				$("span.cross").css("transform","rotate(0)")
+				//$(".pictureslider").addClass("scrollable")
+			
+			} else {
+				
+				$(this).attr('data-click-state', 1)
+				$(this).css("height","100%")
+				$(".pictureslider").hide();
+				$(".description p").show();	
+				
+				
+				//$(this).addClass("equalize")
+				//$(".pictureslider").addClass("equalize")
+				//$(".pictureslider").removeClass("scrollable")
+				$("span.cross").css("transform","rotate(45deg)")
+				//$(".description p").delay(800).fadeIn();
+				//$(".title").css("font-size","8vw")
+				//$(".about").removeClass("shrink")
+    	
+				}
+			
+			
+		}
 
 	});
 	
 //RESIZE HEADER
-	$(".scrollable").scroll(function(){
+	
+	//MOBILE VS DESKTOP
+	
+			
+			$(".scrollable").scroll(function(){
 		
-		var scroller = $(".scrollable").scrollTop();
-		console.log(scroller);
-		
-		if (scroller > 0) { 
-			$(".header").css("font-size","2.5vw")
-			$(".about").addClass("shrink")
-			
-			$(".title.button").css("transform", "translate(-500px, 0px)")
-			
-			
-		
-			} else {
-			$(".title.button").css("transform", "translate(0px, 0px)")
-			
-			$(".header").css("font-size","8vw")
-			$(".about").removeClass("shrink")
+				var scroller = $(".scrollable").scrollTop();
+				console.log(scroller);
 				
+				if($(window).width()>900){
 		
-		}
-	})
+					if (scroller > 0) { 
+						$(".header").css("font-size","2.5vw")
+						$(".about").addClass("shrink")
+			    	
+						$(".title.button").css("transform", "translate(-500px, 0px)")
+			    	
+			    	
+		        	
+						} else {
+						$(".title.button").css("transform", "translate(0px, 0px)")
+			    	
+						$(".header").css("font-size","8.5vw")
+						$(".about").removeClass("shrink")
+					
+		        	
+					}
+				} else {
+					$(".header").css("font-size","8.5vw")
+				}
+			})
+			
+
+		
+		
+
+	
+		
+		
+		
+
 	
 	$(document).scroll(function(){
 		
